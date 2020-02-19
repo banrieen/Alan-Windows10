3d3b87384683a3f0b2fe68ff2c5878d670354420
Banrieen <banrieen@163.com>
2020-02-19 22:32:25 +0800

    Init project

---
+++ App.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "App.xaml.g.h"
+
+namespace winrt::Alan_Windows10::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const&);
+        void OnSuspending(IInspectable const&, Windows::ApplicationModel::SuspendingEventArgs const&);
+        void OnNavigationFailed(IInspectable const&, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs const&);
+    };
+}
